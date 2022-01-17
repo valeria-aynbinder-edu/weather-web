725524b6c564b1e46126bb6926f48dbd9e26e9d2
Valeria Aynbinder <valeria@deeptalk-ai.com>
2022-01-17 16:41:28 +0200

    some additions after the lesson of Jan 17
    Signed-off-by: Valeria Aynbinder <valeria@deeptalk-ai.com>

--- weather_scripts.js
+++ weather_scripts.js
@@ -2,6 +2,8 @@ GO_BTN_ID = "go_button"
 CITY_INPUT_ID = 'city-input'
 WEATHER_DATA_DIV = 'weather-data'
 WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?appid=91201722ff1cc4a99e9c870bbbe2aafb&units=metric&q="
+
+//cities cache
 displayingCities = []
 
 function onCityKeyPress(e) {
@@ -10,6 +12,10 @@ function onCityKeyPress(e) {
         console.log("Detected Enter press")
         sendRequest()
     }
+}
+
+function onCityKeyDown(e) {
+    console.log("Called onCityKeyDown", e.keyCode)
 }
 
 function onCityInput(e) {
@@ -29,6 +35,8 @@ function getGoBtn() {
 }
 
 function getIconUrl(iconCode) {
+    //f"city{}dsjhsd"
+    //return "http://openweathermap.org/img/wn/" + iconCode + "@2x.png"
     return `http://openweathermap.org/img/wn/${iconCode}@2x.png`
 }
 
@@ -36,23 +44,41 @@ function createCityBox(city, icon, temp) {
     // we want to dynamically build this element
     // <div class="weather-data-div">
     //     <h3>London</h3>
-    //     <div style="justify-content: center; align-items: center;">
+    //     <div class="city-inner-div">
     //         <img src="http://openweathermap.org/img/wn/10d@2x.png">
     //         <h2>5°</h2>
     //     </div>
     // </div>
+
+    // <div></div>
     cityDiv = document.createElement("div")
+
+    // <div class="weather-data-div"
     cityDiv.className = "weather-data-div"
+
+    // <h3></h3>
     cityNameh3 = document.createElement("h3")
-    cityNameh3.appendChild(document.createTextNode(city))
+    
+    // London
+    cityTextNode = document.createTextNode(city)
+
+    // <h3>London</h3>
+    cityNameh3.appendChild(cityTextNode)
+
+    // <div></div>
     innerDiv = document.createElement("div")
     innerDiv.className = 'city-inner-div'
+
+
     iconImg = document.createElement("img")
     iconImg.src = getIconUrl(icon)
+
     tempH2 = document.createElement("h2")
     tempH2.appendChild(document.createTextNode(temp + "°"))
+
     innerDiv.appendChild(iconImg)
     innerDiv.appendChild(tempH2)
+
     cityDiv.appendChild(cityNameh3)
     cityDiv.appendChild(innerDiv)
 
@@ -79,17 +105,30 @@ function updateWeatherDetails(city, newIcon, newTemp) {
     }
 }
 
+function responseHandler(response) {
+    if (!response.ok) {
+        throw new Error('Network response was not OK');
+    }
+    return response.json()
+}
+
 function sendRequest() {
+    
+    console.log("inside onclick")
     city = getCityName()
     console.log('Sending request for ', city)
 
     // disable Go button
     getGoBtn().disabled = true
 
+    // solution for ex. 1
+    document.getElementById(CITY_INPUT_ID).value = ""
+
     // add spinner
 
     //send request using fetch + promise
     fetch(WEATHER_URL + city)
+    // .then(responseHandler)
     .then(response => {
         if (!response.ok) {
             throw new Error('Network response was not OK');
@@ -105,6 +144,7 @@ function sendRequest() {
         icon = jsonContent.weather[0].icon
         temp = Math.round(jsonContent.main.temp)
 
+        //if city in displayingCities
         if (displayingCities.includes(city)) {
             // update existing box
             updateWeatherDetails(city, icon, temp)
@@ -116,5 +156,41 @@ function sendRequest() {
     .catch(error => {
         console.error('There has been a problem with your fetch operation:', error);
     });
-
+    console.log(displayingCities)
 }
+
+async function sendRequestWithAsync() {
+    console.log("inside onclick")
+    city = getCityName()
+    console.log('Sending request for ', city)
+
+    // disable Go button
+    getGoBtn().disabled = true
+
+    //waits for response to arrive, meanwhile returns control to UI thread
+    try {
+            const response = await fetch(WEATHER_URL + city);
+            if (!response.ok) {
+                //waits to load json, after request arrived, meanwhile returns control to UI thread
+                
+                const jsonContent = await response.json();
+                console.log("Received response for city ", city)
+                console.log(jsonContent)
+
+                // get needed data
+                weather_desc = jsonContent.weather[0].main
+                icon = jsonContent.weather[0].icon
+                temp = Math.round(jsonContent.main.temp)
+
+                if (displayingCities.includes(city)) {
+                    // update existing box
+                    updateWeatherDetails(city, icon, temp)
+                } else {
+                    // draw a new box
+                    createCityBox(city, icon, temp)
+                }
+            }
+    } catch(error) {
+        console.log('ERROR!!!!', error)
+    }
+  }
